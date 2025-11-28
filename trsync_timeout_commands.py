509cdeff7c4052355162593be4313523bd5ba8f4
sskki-exe <harrychoi@sskki.fyi>
2025-11-28 22:59:51 +1100

    gcode?

--- trsync_timeout_commands.py
+++ trsync_timeout_commands.py
@@ -20,17 +20,20 @@ class TrsyncTimeoutCommands:
         gcode.register_command("TRSYNC_TIMEOUT_RESET", self.cmd_TRSYNC_TIMEOUT_RESET,
                                     desc=self.cmd_TRSYNC_TIMEOUT_RESET_help)
         
-    cmd_TRSYNC_TIMEOUT_QUERY_help = ("Returns current TRSYNC_TIMEOUT value")
+    cmd_TRSYNC_TIMEOUT_QUERY_help = "Returns current TRSYNC_TIMEOUT value"
     def cmd_TRSYNC_TIMEOUT_QUERY(self, gcmd):
-        gcmd.respond_info("TRSYNC_TIMEOUT = %.3f" % (0.025))
+        gcode = self.printer.lookup_object('gcode')
+        gcode.respond_info("TRSYNC_TIMEOUT = %.3f" % (0.025))
     
-    cmd_TRSYNC_TIMEOUT_SET_help = ("Sets new TRSYNC_TIMEOUT value")
+    cmd_TRSYNC_TIMEOUT_SET_help = "Sets new TRSYNC_TIMEOUT value"
     def cmd_TRSYNC_TIMEOUT_SET(self, gcmd):
-        gcmd.respond_info("Set TRSYNC_TIMEOUT = %.3f" % (0.025))
+        gcode = self.printer.lookup_object('gcode')
+        gcode.respond_info("Set TRSYNC_TIMEOUT = %.3f" % (0.025))
     
-    cmd_TRSYNC_TIMEOUT_RESET_help = ("Resets TRSYNC_TIMEOUT to default value (0.025)")
+    cmd_TRSYNC_TIMEOUT_RESET_help = "Resets TRSYNC_TIMEOUT to default value (0.025)"
     def cmd_TRSYNC_TIMEOUT_RESET(self, gcmd):
-        gcmd.respond_info("Reset TRSYNC_TIMEOUT = %.3f" % (0.025))
+        gcode = self.printer.lookup_object('gcode')
+        gcode.respond_info("Reset TRSYNC_TIMEOUT = %.3f" % (0.025))
 
 def load_config(config):
     return TrsyncTimeoutCommands(config)
